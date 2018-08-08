423bf560bae76d87b3bcb3e8db6cec7647bb0c24
Samrita Bakshi <samritabakshi@Samritas-MacBook-Pro.local>
2018-08-08 18:31:31 +0530

    otp changes

--- lib/casino/activerecord_authenticator.rb
+++ lib/casino/activerecord_authenticator.rb
@@ -45,9 +45,9 @@ class CASino::ActiveRecordAuthenticator
     user = @model.send("find_by_#{@options[:username_column]}!", username)
     p user
     p @options[:password_column]
-    p user.send(@options[:password_column])
-    col_name = @options[:password_column]
-    password_from_database = user.send(:col_name)
+    password_from_database = user.send(@options[:password_column]) if authenticator_name != "auth_user_by_otp"
+    password_from_database = user.send(:value) if authenticator_name == "auth_user_by_otp"
+    p "after pfd"
     p password_from_database
     if authenticator_name == "auth_user_by_otp"
       p "in OTP auth"
